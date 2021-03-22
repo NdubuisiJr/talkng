342edbbef538e8fe5fe3c00b00f485500a511158
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-03-22 19:51:09 +0100

    modified app start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,10 @@
 
 sudo chmod -R 777 /home/ec2-user/talkng
 
+cd /hom/ec2-user/talkng
+
 docker run -d --name talk \
 -p 3000:3000 -p 9000:9000 \
---mount type=bind,source="$(pwd)/certs/server.key",target="/src/certs/server.key" \
---mount type=bind,source="$(pwd)/certs/server.cert",target="/src/certs/server.cert" \
+--mount type=bind,source="$(pwd)/server.key",target="/src/certs/server.key" \
+--mount type=bind,source="$(pwd)/server.cert",target="/src/certs/server.cert" \
  ndubuisijr/talkng
