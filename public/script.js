f7b869ce5cd79a80de4a8c4310510bf146503961
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-03-22 08:41:28 +0100

    added initial files for developement

---
+++ public/script.js
@@ -0,0 +1,74 @@
+//const socket = io('/');
+var socket = io('/',{ transports: ['websocket'], rejectUnauthorized: false });
+
+const peer = new Peer({
+    host: '/',
+    port: '9000',
+});
+
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+const getusermedia = navigator.mediaDevices.getUserMedia;// || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+getusermedia({video: true, audio: true})
+.then(stream => {
+    myVideo.classList.add('small-video');
+    addVideoStream(myVideo, stream);
+})
+.catch(err =>{
+    console.log(err);
+});
+
+peer.on('open', id =>{
+    console.log('opening');
+    socket.emit('join-room', ROOM_ID, id);
+});
+
+socket.on('user-joined', userId =>{
+    console.log('calling ', userId);
+    getusermedia({video: true, audio: true})
+    .then(stream =>{
+      var call = peer.call(userId, stream);
+      hookUpEvents(call);
+    })
+    .catch(err=> {
+      console.log('Failed to get local stream' ,err);
+    });
+});
+
+peer.on('call', (call) => {
+    getusermedia({video: true, audio: true})
+    .then(stream => {
+        call.answer(stream); 
+        hookUpEvents(call);
+  }).catch (err => {
+    console.log('Failed to get local stream' ,err);
+  });
+});
+
+
+const addVideoStream = (video, stream )=>{
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', ()=>{
+        video.play();
+    });
+    videoGrid.append(video);
+};
+
+const hookUpEvents = (call)=>{
+    call.on('stream', (remoteStream) =>{
+        console.log(remoteStream);
+        const existing = document.getElementById(remoteStream.id);
+        if(!existing){
+            const myVideo2 = document.createElement('video');
+            myVideo2.id=remoteStream.id;
+            addVideoStream(myVideo2, remoteStream);
+        }
+    });
+    
+    call.on('close', ()=>{
+        myVideo2.remove();
+    });
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const sio = require('socket.io');
+const { v4: uuidV4 } = require('uuid');
+const fs = require('fs');
+
+const app = express();
+const server = require('https').createServer({
+    key: fs.readFileSync(__dirname+'\\certs\\server.key'),
+    cert: fs.readFileSync(__dirname+'\\certs\\server.cert'),
+    requestCert: true,
+    rejectUnauthorized: false
+},app);
+const io = sio(server); 
+
+
+app.set('view engine', 'ejs'); 
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`);
+});
+
+app.get('/:id', (req, res)=>{
+    res.render('room',{roomId: req.params.id});
+});
+
+io.on('connection', socket =>{
+    socket.on('join-room', (roomId, userId)=>{
+        socket.join(roomId);
+        socket.to(roomId).emit('user-joined', userId);
+    });
+});
+
+server.listen(3000, ()=>{
+    console.log('server listening on port 3000');
+});
